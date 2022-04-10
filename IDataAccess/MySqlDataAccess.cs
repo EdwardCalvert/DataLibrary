83b666383ba92e7088d929a7177a57aaf492ad5c
Ed Calvert <72658447+EdwardCalvert@users.noreply.github.com>
2022-04-10 07:26:56 +0100

    Add project files.

---
+++ IDataAccess/MySqlDataAccess.cs
@@ -0,0 +1,30 @@
+﻿using Dapper;
+using MySql.Data.MySqlClient;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DataLibrary
+{
+    public class MySqlDataAccess : IDataAccess
+    {
+        public async Task<List<U>> LoadData<U, T>(string sql, T Parameters, string connectionString)
+        {
+            using (IDbConnection connection = new MySqlConnection(connectionString))
+            {
+                var rows = await connection.QueryAsync<U>(sql, Parameters);
+
+                return rows.ToList();
+            }
+        }
+
+        public Task SaveData<T>(string sql, T Parameters, string connectionString)
+        {
+            using (IDbConnection connection = new MySqlConnection(connectionString))
+            {
+                return connection.ExecuteAsync(sql, Parameters);
+            }
+        }
+    }
+}
