1191ccd71379c0f122adb53dc5f5099d1c846fe8
Ed Calvert <aguy@domain.com>
2022-11-15 18:52:18 +0000

    Added SqlLite Support

---
+++ IDataAccess/SqliteDataAccess.cs
@@ -0,0 +1,45 @@
+﻿using Dapper;
+using DataLibrary;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EdCalvert.DataLibrary
+{
+    public class SqliteDataAccess :IDataAccess
+    {
+        public async Task<List<U>> LoadMultiple<U, T>(string sql, T Parameters, string connectionString)
+        {
+            using (IDbConnection connection = new SQLiteConnection(connectionString))
+            {
+                var rows = await connection.QueryAsync<U>(sql, Parameters);
+
+                return rows.ToList();
+            }
+        }
+
+        public async Task<U> LoadSingle<U, T>(string sql, T Parameters, string connectionString)
+        {
+            using (IDbConnection connection = new SQLiteConnection(connectionString))
+            {
+                var rows = await connection.QueryAsync<U>(sql, Parameters);
+
+                return rows.Single();
+            }
+        }
+
+        public Task SaveData<T>(string sql, T Parameters, string connectionString)
+        {
+            using (IDbConnection connection = new SQLiteConnection(connectionString))
+            {
+                return connection.ExecuteAsync(sql, Parameters);
+            }
+        }
+
+    }
+}
